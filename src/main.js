abb13888bf63d6e9b7aefdcf007a90caf08064a6
Nikulichka <poteryaevainna@gmail.ru>
2021-10-29 10:08:24 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,94 @@
+import SiteMenuView from './view/site-menu.js';
+import StatisticsView from './view/statistics.js';
+import BoardPresenter from './presenter/board.js';
+import FilterPresenter from './presenter/filter.js';
+import TasksModel from './model/tasks.js';
+import FilterModel from './model/filter.js';
+import {isOnline} from './utils/common.js';
+import {render, RenderPosition, remove} from './utils/render.js';
+import {toast} from './utils/toast.js';
+import {MenuItem, UpdateType, FilterType} from './const.js';
+import Api from './api/api.js';
+import Store from './api/store.js';
+import Provider from './api/provider.js';
+
+const AUTHORIZATION = 'Basic hS2sd3dfSwcl1sa2j';
+const END_POINT = 'https://14.ecmascript.pages.academy/task-manager';
+const STORE_PREFIX = 'taskmanager-localstorage';
+const STORE_VER = 'v14';
+const STORE_NAME = `${STORE_PREFIX}-${STORE_VER}`;
+
+const siteMainElement = document.querySelector('.main');
+const siteHeaderElement = siteMainElement.querySelector('.main__control');
+
+const api = new Api(END_POINT, AUTHORIZATION);
+const store = new Store(STORE_NAME, window.localStorage);
+const apiWithProvider = new Provider(api, store);
+const tasksModel = new TasksModel();
+const filterModel = new FilterModel();
+
+const siteMenuComponent = new SiteMenuView();
+const boardPresenter = new BoardPresenter(siteMainElement, tasksModel, filterModel, apiWithProvider);
+const filterPresenter = new FilterPresenter(siteMainElement, filterModel, tasksModel);
+
+const handleTaskNewFormClose = () => {
+  siteMenuComponent.getElement().querySelector(`[value=${MenuItem.TASKS}]`).disabled = false;
+  siteMenuComponent.setMenuItem(MenuItem.TASKS);
+};
+
+let statisticsComponent = null;
+
+const handleSiteMenuClick = (menuItem) => {
+  switch (menuItem) {
+    case MenuItem.ADD_NEW_TASK:
+      remove(statisticsComponent);
+      boardPresenter.destroy();
+      filterModel.setFilter(UpdateType.MAJOR, FilterType.ALL);
+      boardPresenter.init();
+      if (!isOnline()) {
+        toast('You can\'t create new task offline');
+        siteMenuComponent.setMenuItem(MenuItem.TASKS);
+        break;
+      }
+      boardPresenter.createTask(handleTaskNewFormClose);
+      siteMenuComponent.getElement().querySelector(`[value=${MenuItem.TASKS}]`).disabled = true;
+      break;
+    case MenuItem.TASKS:
+      boardPresenter.init();
+      remove(statisticsComponent);
+      break;
+    case MenuItem.STATISTICS:
+      boardPresenter.destroy();
+      statisticsComponent = new StatisticsView(tasksModel.getTasks());
+      render(siteMainElement, statisticsComponent, RenderPosition.BEFOREEND);
+      break;
+  }
+};
+
+filterPresenter.init();
+boardPresenter.init();
+
+apiWithProvider.getTasks()
+  .then((tasks) => {
+    tasksModel.setTasks(UpdateType.INIT, tasks);
+    render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
+    siteMenuComponent.setMenuClickHandler(handleSiteMenuClick);
+  })
+  .catch(() => {
+    tasksModel.setTasks(UpdateType.INIT, []);
+    render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
+    siteMenuComponent.setMenuClickHandler(handleSiteMenuClick);
+  });
+
+window.addEventListener('load', () => {
+  navigator.serviceWorker.register('/sw.js');
+});
+
+window.addEventListener('online', () => {
+  document.title = document.title.replace(' [offline]', '');
+  apiWithProvider.sync();
+});
+
+window.addEventListener('offline', () => {
+  document.title += ' [offline]';
+});
---
+++ src/utils/task.js
@@ -0,0 +1,63 @@
+import dayjs from 'dayjs';
+
+export const isTaskExpired = (dueDate) => {
+  return dueDate === null ? false : dayjs().isAfter(dueDate, 'D');
+};
+
+export const isTaskExpiringToday = (dueDate) => {
+  return dueDate === null ? false : dayjs(dueDate).isSame(dayjs(), 'D');
+};
+
+export const isTaskRepeating = (repeating) => {
+  return Object.values(repeating).some(Boolean);
+};
+
+export const formatTaskDueDate = (dueDate) => {
+  if (!dueDate) {
+    return '';
+  }
+
+  return dayjs(dueDate).format('D MMMM');
+};
+
+// Функция помещает задачи без даты в конце списка,
+// возвращая нужный вес для колбэка sort
+const getWeightForNullDate = (dateA, dateB) => {
+  if (dateA === null && dateB === null) {
+    return 0;
+  }
+
+  if (dateA === null) {
+    return 1;
+  }
+
+  if (dateB === null) {
+    return -1;
+  }
+
+  return null;
+};
+
+export const sortTaskUp = (taskA, taskB) => {
+  const weight = getWeightForNullDate(taskA.dueDate, taskB.dueDate);
+
+  if (weight !== null) {
+    return weight;
+  }
+
+  return dayjs(taskA.dueDate).diff(dayjs(taskB.dueDate));
+};
+
+export const sortTaskDown = (taskA, taskB) => {
+  const weight = getWeightForNullDate(taskA.dueDate, taskB.dueDate);
+
+  if (weight !== null) {
+    return weight;
+  }
+
+  return dayjs(taskB.dueDate).diff(dayjs(taskA.dueDate));
+};
+
+export const isDatesEqual = (dateA, dateB) => {
+  return (dateA === null && dateB === null) ? true : dayjs(dateA).isSame(dateB, 'D');
+};
